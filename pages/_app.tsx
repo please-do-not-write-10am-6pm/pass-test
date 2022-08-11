3d6553d74567aa0a44c3b1166fff938c31038869
please-do-not-write-10am-6pm <nothingpermanent0304@gmail.com>
2022-08-10 19:12:45 -0700

    added: navbar and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "../src/components/navbar";
+import Footer from "../src/components/footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,47 @@
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function SmallCentered() {
+  return (
+    <Box background={"black"}>
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        py={4}
+        spacing={4}
+        justify={"center"}
+        align={"center"}
+      >
+        <Text fontSize={"1.2rem"} fontWeight={"700"} color={"white"}>
+          @ 2022 TALLYSIGHT INC. RESERVED
+        </Text>
+      </Container>
+
+      <Box
+        borderTopWidth={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
+      >
+        <Container
+          as={Stack}
+          maxW={"6xl"}
+          py={4}
+          direction={{ base: "column", md: "row" }}
+          spacing={4}
+          justify={{ base: "center", md: "center" }}
+          align={{ base: "center", md: "center" }}
+        >
+          <Text color={"white"}>
+            If you or someone you know has a gambling problem and wants help,
+            call 1800 Gambler. You must be 21 years or older to place a bet
+          </Text>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
